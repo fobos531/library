c002c8dce03a4cbf092422d648e0f5ef1dcfdf69
Jakov Glavina <cyber.shot.jakov@gmail.com>
2019-09-11 07:43:33 +0200

    add removeBook, toggleReadStatus, improve render() functionality

--- script.js
+++ script.js
@@ -10,14 +10,23 @@ const container = document.getElementById("container");
 
 function render() {
     container.innerHTML = "";
+    myLibrary = myLibrary.map(elem => {
+        elem.index = myLibrary.indexOf(elem); //pri svakom renderiranju svakoj knjizi stavi/updateaj index
+        return elem;
+    })
     myLibrary.forEach(elem => {
         const div = document.createElement("div");
-        div.innerText = "";
-        for (let key in elem) {
-            div.innerHTML += elem[key] + "<br>";
-        }
         div.classList.add("element");
         container.appendChild(div);
+        for (let key in elem) {
+            if (key == "index") continue;
+            const span = document.createElement("span");
+            span.innerText = elem[key];
+            div.appendChild(span);
+        }
+        const index = myLibrary.indexOf(elem);
+        div.innerHTML += `<button onclick="removeBook(${index})">Remove book</button>`;
+        div.innerHTML += `<button onclick="toggleReadStatus(${index})">Toggle read status</button>`;
     })
 }
 
@@ -28,6 +37,19 @@ function addBookToLibrary() {
     book.push(prompt("Enter the number of pages"));
     book.push(prompt("Have you read the book yet? Answer with either 'yes' or 'no'"));
     myLibrary.push(new Book(book));
+    render();
+}
+
+function removeBook(index) {
+    myLibrary.splice(index, 1);
+    render();
+}
+
+function toggleReadStatus(index) {
+    if(myLibrary[index].read == "yes") {
+        myLibrary[index].read = "no";
+    }
+    else myLibrary[index].read = "yes";
     render();
 }
 
